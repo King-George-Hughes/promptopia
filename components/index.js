13e2122591e5cffca42973f493e471bc67fbf681
unknown <hughesgeorgeking@gmail.com>
2023-06-24 08:40:41 +0000

    first commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import Feeds from "./Feeds";
+import Form from "./Form";
+import NavBar from "./NavBar";
+import Profile from "./Profile";
+import PromptCard from "./PromptCard";
+import Provider from "./Provider";
+
+export { Feeds, Form, NavBar, Profile, PromptCard, Provider };
